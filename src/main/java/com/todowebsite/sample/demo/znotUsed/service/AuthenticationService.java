20cb52694a87790e4f2dd6f4b28a4cce794cb4b1
TheOmegaVencedora <dikaneyang@gmail.com>
2025-06-18 12:23:56 +0100

    update commit

--- src/main/java/com/todowebsite/sample/demo/znotUsed/service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package com.todowebsite.sample.demo.znotUsed.service;
-
-import com.todowebsite.sample.demo.entity.Users;
-
-public interface AuthenticationService {
-
-    public boolean authenticate(Users users);
-}
